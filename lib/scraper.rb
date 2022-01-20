408b65a17acbda97b3cb7a9bc2072cf2ab5ce4de
Michael Lobman <michael.lobman@gmail.com>
2022-01-20 10:47:00 -0500

    Complete assignment

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,17 @@ require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+
+doc = Nokogiri::HTML(html)
+
+courses = doc.css(".heading-35-semibold color-teal mb-20").text.strip
+
+courses.each do |course|
+    puts course.text.strip
+end
+
+p doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")[0]
+
+p doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")[0].name
+
+p doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")[0].attributes
